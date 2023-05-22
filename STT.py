06b0050cf2437fd6e95d522f9d21d6a58befcc3e
Pratham Sharma <sharma.pratham0502@gmail.com>
2023-05-22 15:05:40 -0230

    added info passage checks

--- STT.py
+++ STT.py
@@ -2,6 +2,7 @@ import whisper
 
 def transcribe(audio_file, language):
     model = whisper.load_model("small")
+    print("Sending Audio to Whisper...")
     result = model.transcribe(audio_file, language=language)
     return result["text"]
 
