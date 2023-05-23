1845e034a653f6947e5067d2ecdffcc4763b7957
Pratham Sharma <sharma.pratham0502@gmail.com>
2023-05-23 16:50:58 -0230

    tts added and updated the UI

--- GUI.py
+++ GUI.py
@@ -1,30 +1,36 @@
 import threading
-from tkinter import Tk, Button, OptionMenu, Text, StringVar
+import tkinter as tk
+from tkinter import Tk, Button, OptionMenu, Text, StringVar, messagebox
 from recorder import Recorder
-from STS import transcribe
+from STS import transcribe, text_to_speech, audioPlayer
 from translator import translate
-from chatbot import get_ai_response, separate_sentences
+from chatbot import get_ai_response
 
-class Application(Tk):
-    def __init__(self):
-        Tk.__init__(self)
+class Application:
+    def __init__(self, window):
+        self.window = window
+        self.window.title("My Companion")
 
         # Create an instance of the Recorder class
         self.recorder = Recorder()
+        
+        # Create TextEdit to display output
+        self.text_output = tk.Text(self.window, width=50, height=25)
+        self.text_output.grid(row=0, column=0, rowspan=6, padx=5, pady=5)
 
         # Create record button
-        self.record_button = Button(self, text="Start Recording", command=self.record)
-        self.record_button.pack()
+        self.record_button = tk.Button(self.window, text='Start Recording', command=self.record)
+        self.record_button.grid(row=0, column=1, padx=5, pady=5)
+        
+        # Create ComboBox for language selection
+        self.lang_select = tk.StringVar(self.window)
+        self.lang_select.set("EN") # default value
+        lang_menu = tk.OptionMenu(self.window, self.lang_select, "EN", "JA")
+        lang_menu.grid(row=1, column=1, padx=5, pady=5)
 
-        # Create Dropdown menu for language selection
-        self.lang_select = StringVar(self)
-        self.lang_select.set("EN")  # default value
-        self.option_menu = OptionMenu(self, self.lang_select, "EN", "JA")
-        self.option_menu.pack()
-
-        # Create Text widget to display output
-        self.text_output = Text(self)
-        self.text_output.pack()
+        # Create a label below the record button
+        self.instruction_label = tk.Label(self.window, text="")
+        self.instruction_label.grid(row=2, column=1, padx=5, pady=5)
 
     def record(self):
         # Call your record function in a separate thread
@@ -32,10 +38,11 @@ class Application(Tk):
         record_thread.start()
 
     def record_audio(self):
+        self.instruction_label['text'] = "Press and hold 'space' key to start recording..."
         try:
             # Record the audio
             self.recorder.record_audio(output_filename="output/output.wav")
-            
+            self.instruction_label['text'] = ""  # clear the instruction label            
             # Transcribe automatically
             self.text = transcribe("output/output.wav", self.lang_select.get())
             
@@ -47,7 +54,7 @@ class Application(Tk):
                 
             get_ai_response(translated_text)
             try:
-                with open('output/AI_response.txt', 'r') as file:
+                with open('output/AI_response.txt', 'r', encoding="utf-8") as file:
                     ai_response = file.read()
             except FileNotFoundError:
                 print("AI_response.txt not found.")
@@ -55,14 +62,16 @@ class Application(Tk):
                 print(f"An error occurred: {e}")
             ai_response = translate(ai_response, "JA")
             with open('output/AI_response.txt', 'w', encoding="utf-8") as file:
-                separated_text = separate_sentences(ai_response)
-                file.write(separated_text)
-
+                file.write(ai_response)
+            text_to_speech("output/AI_response.txt")
+            audioPlayer("output/audioResponse.wav")
             # Display the translated text
             self.text_output.insert('1.0', ai_response)
+            
         except Exception as e:
             print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    app = Application()
-    app.mainloop()
+    window = tk.Tk()
+    app = Application(window)
+    window.mainloop()
--- STS.py
+++ STS.py
@@ -1,8 +1,54 @@
 import whisper
+import requests
+import os
+from pydub import AudioSegment
+from pydub.playback import play
+from dotenv import load_dotenv
+load_dotenv()
 
 def transcribe(audio_file, language):
     model = whisper.load_model("small")
     print("Sending Audio to Whisper...")
     result = model.transcribe(audio_file, language=language)
     return result["text"]
+
+def text_to_speech(text_file):
+    try:
+        with open(text_file, 'r', encoding="utf-8") as file:
+            text = file.read()
+    except FileNotFoundError:
+        print("AI_response.txt not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    voicevox_api_key = os.getenv('VOICEVOX_API_KEY')
+    VOICEVOX_URL=f"https://deprecatedapis.tts.quest/v2/voicevox/audio/?key={voicevox_api_key}&text={text}"
+    
+    try:
+        response = requests.request(
+            "POST", VOICEVOX_URL)
+         # Handle specific error messages from the API
+        if response.text == "invalidApiKey":
+            print("Invalid API key provided.")
+        elif response.text == "failed":
+            print("Synthesis failed.")
+        elif response.text == "notEnoughPoints":
+            print("Insufficient points.")
+    except requests.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"An error occurred: {err}")
+        
+    print(f'response: {response}')
+    wav_bytes = None
+    wav_bytes = response.content
+    with open("output/audioResponse.wav", "wb") as file:
+        file.write(wav_bytes)
+        
+def audioPlayer(audio_file):
+    # Load .wav file
+    audio = AudioSegment.from_wav(audio_file)
+
+    # Play the audio file
+    play(audio)
+    
 
